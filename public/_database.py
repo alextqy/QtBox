b8d564082e99c868b0586731f3a212546290e0a2
alextqy <289959263@qq.com>
2022-01-13 17:27:10 +0800

    update

---
+++ public/_database.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+from public._base import *
+import sqlite3
+
+
+class Database():
+    # DBFileURL = os.path.join(os.getcwd()) + "/DBRoom.db"
+
+    def __init__(self, DBRoomPath):
+        super().__init__()
+        self.Init()
+        self.DBFileURL = DBRoomPath
+
+    def Init(self):
+        if not isfile(self.DBFileURL):
+            file = open(self.DBFileURL, "w")
+            file.close()
+        self.SqlCon = sqlite3.connect(self.DBFileURL)
+        self.Cur = self.SqlCon.cursor()
+
+    def Command(self, CommandParam):
+        return self.Cur.execute(CommandParam)
+
+    def Tran(self):
+        self.Cur.execute("BEGIN TRANSACTION")
+
+    def Rollback(self):
+        self.Cur.execute("ROLLBACK")
+
+    def Commit(self):
+        self.Cur.execute("COMMIT")
